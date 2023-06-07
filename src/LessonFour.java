abff27d6ec24bdf744b65a8312e083b4f0b6ff9c
Valeriia Vovk <vovkvaleriya15@gmail.com>
2023-06-07 22:27:45 +0300

    Operations with String

---
+++ src/LessonFour.java
@@ -0,0 +1,13 @@
+package src;
+
+public class LessonFour {
+
+    public static void main(String[] args) {
+        String name = "Valeriia";
+        System.out.println(name.toLowerCase());
+        System.out.println(name.toUpperCase());
+        System.out.println(name.charAt(0));
+        System.out.println(name.charAt(name.length()-1));
+
+    }
+}
